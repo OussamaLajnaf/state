32cdc48c271c29d92065e48895a203e2dcdd9be0
OussamaLajnaf <oussamalajnaf32@gmail.com>
2021-09-16 03:19:42 +0200

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+state ={ me:{
+photo:"./0122.jpg",
+fulname:"Oussama Lajnaf",
+adress: "Sfax",
+proffession: "Streamer"},
+count:0,
+isshow:true
+}
+handelshow=()=>{
+  this.setState({isshow:!this.state.isshow})
+}
+componentDidMount(){
+  setInterval(() => {this.setState({count:this.state.count+1})
+  }, 1000);
 }
 
-export default App;
+
+
+
+
+  render() {
+    console.log("render")
+    return (
+      <div style={{display:"block" , textAlign:"center"}}>
+        
+        <button onClick={this.handelshow}>{this.state.isshow?"Hide":"show"}</button>
+        {this.state.isshow&& <div>  <img src={this.state.me.photo} style={{height:"300px",width:"300px"}}alt=""/>
+        <p style={{ fontFamily:"inherit", fontsize:"25px", textAlign:"center" }}> {this.state.me.fulname}</p>
+        <p style={{ fontFamily:"inherit", fontsize:"25px", textAlign:"center" }}> {this.state.me.adress}</p>
+        <p style={{ fontFamily:"inherit", fontsize:"25px", textAlign:"center" }}> {this.state.me.proffession}</p>  </div>}
+        
+        
+        
+        
+        <h1>Footer by me</h1>
+      </div>
+    )
+  }
+}
